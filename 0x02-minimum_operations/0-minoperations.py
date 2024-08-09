695a27f42327560134b4dbb6738f2adf64164db6
Jude Osam-Sackey <43099552+judeo-s@users.noreply.github.com>
2024-08-09 05:05:12 +0000

    i have no idea why the checker keeps flagging me

--- 0x02-minimum_operations/0-minoperations.py
+++ 0x02-minimum_operations/0-minoperations.py
@@ -4,8 +4,7 @@
 A python script to solve the "H" file manipulation challenge!
 """
 
-
-def minOperations(n):
+def minOperations(n: int) -> int:
     """
     A method that calculates the fewest number of operations needed to result
     in exactly n H characters in a file
