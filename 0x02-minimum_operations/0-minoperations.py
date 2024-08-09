a5c21196eba665f585c9685ee6f831f982953404
Jude Osam-Sackey <43099552+judeo-s@users.noreply.github.com>
2024-08-09 05:00:04 +0000

    fixed a few things with the script

--- 0x02-minimum_operations/0-minoperations.py
+++ 0x02-minimum_operations/0-minoperations.py
@@ -4,11 +4,13 @@
 A python script to solve the "H" file manipulation challenge!
 """
 
-def minOperations(n)
+
+def minOperations(n):
     """
     A method that calculates the fewest number of operations needed to result
     in exactly n H characters in a file
     """
+
     if n == 1:
         return 0  # No operations needed for one "H"
 
