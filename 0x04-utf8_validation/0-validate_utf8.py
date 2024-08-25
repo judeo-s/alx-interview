200289cf5aaf6c4df9af75fadb0c9bd78d3ebb53
demon <demon@localhost.localdomain>
2024-08-25 20:38:30 +0000

    feat: created a method that validates if a data is UTF-8 encoded

---
+++ 0x04-utf8_validation/0-validate_utf8.py
@@ -0,0 +1,37 @@
+#!/usr/bin/env python3
+"""
+A module that determines if a given data set represents a valid UTF-8
+encoding
+"""
+
+
+def validUTF8(data) -> bool:
+    """
+    Method that determines if a given data set represents a valid UTF-8
+    encoding.
+
+    Args:
+        - data: list of integers
+
+    Returns:
+        bool: True if data is a valid UTF-8 encoding, else return False
+    """
+    bytes = 0
+    for byte in data:
+        byte = byte & 255
+        if bytes:
+            if byte >> 6 != 2:
+                return False
+            bytes -= 1
+        else:
+            if byte >> 7 == 0:
+                continue
+            elif byte >> 5 == 6:
+                bytes = 1
+            elif byte >> 4 == 14:
+                bytes = 2
+            elif byte >> 3 == 30:
+                bytes = 3
+            else:
+                return False
+    return bytes == 0
