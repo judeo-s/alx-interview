808298fb922be352f8fe9201f84587cb1eae032d
Jude Osam-Sackey <43099552+judeo-s@users.noreply.github.com>
2024-09-05 17:18:26 +0000

    feat: made a few changes to the code

--- 0x06-starwars_api/0-starwars_characters.js
+++ 0x06-starwars_api/0-starwars_characters.js
@@ -1,61 +1,30 @@
 #!/usr/bin/node
 
-// Print the list of characters in a Star Wars movie.
-
+if (process.argv.length !== 3) {
+  process.exit();
+}
+const movieID = process.argv[2];
+const url = `https://swapi-api.alx-tools.com/api/films/${movieID}`;
 const request = require('request');
 
-/**
- * Print the list of characters in a Star Wars movie.
- * @param movieId The id of the movie.
- */
-function getStarWarCharacters (movieId) {
-  if (isNaN(movieId)) {
-    console.error('Invalid movie id');
-    return;
-  }
-
-  if (movieId < 1 || movieId > 7) {
-    console.error('Invalid movie id');
-    return;
-  }
-
-  const url = 'https://swapi-api.alx-tools.com/api/films/' + movieId;
-
-  request(url, (error, response, body) => {
-    if (error) {
-      console.log(error);
-    } else {
-      const filmData = JSON.parse(body);
-      const characters = filmData.characters;
-
-      // occurs when the movieId is invalid
-      if (characters === undefined) {
-        console.error('No characters found');
-        return;
+function printNextCharacter(urls) {
+  const characterURL = urls.shift();
+  if (characterURL) {
+    request(characterURL, function (error, response, body) {
+      if (!error) {
+        console.log(JSON.parse(body).name);
+        printNextCharacter(urls);
       }
+    });
+  }
+}
 
-      const allCharacters = {};
-      characters.forEach(character => {
-        request(character, async (error, response, body) => {
-          if (error) {
-            console.log(error);
-          } else {
-            const characterData = await JSON.parse(body);
-            allCharacters[character] = characterData.name;
-            if (Object.keys(allCharacters).length === characters.length) {
-              characters.forEach(character => {
-                console.log(allCharacters[character]);
-              });
-            }
-          }
-        });
-      });
+function printStarWarsCharacters(url) {
+  request(url, function (error, response, body) {
+    if (!error) {
+      printNextCharacter(JSON.parse(body).characters);
     }
   });
 }
 
-if (process.argv.length === 3) {
-  getStarWarCharacters(process.argv[2]);
-} else {
-  console.error('Usage: ./0-starwars_characters.js <film_id>');
-}
+printStarWarsCharacters(url);
